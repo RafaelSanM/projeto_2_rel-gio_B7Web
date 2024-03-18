0737df4c244694c9d5c4945f353d089645e32bca
Rafael Santilli Machado <rafaelmachado07@yahoo.com>
2024-03-18 17:22:19 +0000

    apagando comentario no js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function updateClock() {
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
-    let sDeg = ((360 / 60) * second) - 90; // 90graus para colocar o ponteiro contando de cima pois ele inicia nos 15 por causa do CSS ver class .p (transform-origin: left -- eixo dele deve ser do lado esquerdo e nao no meio)
+    let sDeg = ((360 / 60) * second) - 90; 
     let mDeg = ((360 / 60) * minute) - 90;
     let hDeg = ((360 / 12) * hour) - 90;
 
@@ -23,12 +23,6 @@ function updateClock() {
 
 function fixZero(time) {
     return time < 10 ? `0${time}` : time;
-
-    // if(time < 10) {
-    //     return '0'+time;
-    // } else {
-    //     return time;
-    // }
 } // essa função corrige o formato de horario quando a hora ou o minuto ou o segundo é menor que 10. Por exemplo: 12hs30min05s sem a função ficaria 12hs30min5s (sem o zero, ou seja, incorreto o formato)
 
 setInterval(updateClock, 1000);
